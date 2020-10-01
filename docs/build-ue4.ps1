5d3e8b93504b96c4062462881baee75b756b2a5a
esplo <esplo@users.noreply.github.com>
2020-10-01 17:33:27 +0900

    modify the UE4 build script example

--- docs/build-ue4.ps1
+++ docs/build-ue4.ps1
@@ -1,3 +1,5 @@
+$ErrorActionPreference = "Stop"
+
 # build against {VCSROOT}\TestProj.uproject
 $PWD = Get-Location
 $ProjectName = "TestProj"
@@ -5,21 +7,24 @@ $ProjectFile = "$PWD\$ProjectName.uproject" # change this into your project file
 $ArtifactBucket = "<your-backet-for-artifact>" # change this into your bucket name for build artifacts
 $ArchiveDir = "C:\archive"
 
-# build Engine
-&"C:\UE4\Engine\Build\BatchFiles\Build.bat" -Target="ShaderCompileWorker Win64 Development" -Target="UE4Editor Win64 Development" -WaitMutex -FromMsBuild
-&"C:\UE4\Engine\Build\BatchFiles\Build.bat" -Target="UnrealPak Win64 Development" -WaitMutex -FromMsBuild
-&"C:\UE4\Engine\Build\BatchFiles\Build.bat" -Target="UE4Game Win64 Shipping" -WaitMutex -FromMsBuild
+# if you need
+# &"C:\UE4\Engine\Build\BatchFiles\Build.bat" -Target="ShaderCompileWorker Win64 Development" -Target="UE4Editor Win64 Development" -WaitMutex -FromMsBuild
+# &"C:\UE4\Engine\Build\BatchFiles\Build.bat" -Target="UnrealPak Win64 Development" -WaitMutex -FromMsBuild
+# &"C:\UE4\Engine\Build\BatchFiles\Build.bat" -Target="UE4Game Win64 Shipping" -WaitMutex -FromMsBuild
 
+if (Test-Path $ArchiveDir) { Remove-Item -Recurse $ArchiveDir }
 mkdir $ArchiveDir
 
-&"C:\UE4\Engine\Binaries\Win64\UE4Editor.exe" $ProjectFile -run=DerivedDataCache -fill
+# if you need
+# Start-Process -FilePath "C:\UE4\Engine\Binaries\Win64\UE4Editor.exe" -ArgumentList "$ProjectFile -run=DerivedDataCache -fill" -Wait -NoNewWindow
+# &"C:\UE4\Engine\Binaries\Win64\UE4Editor-Cmd.exe" $ProjectFile -run=resavepackages -buildlighting -AllowCommandletRendering -Messaging
 
-&"C:\UE4\Engine\Binaries\Win64\UE4Editor-Cmd.exe" $ProjectFile -run=resavepackages -buildlighting -AllowCommandletRendering -Messaging
+&"C:\UE4\Engine\Build\BatchFiles\Build.bat" -Target="UE4Game Win64 Debug" -project="$ProjectFile" -WaitMutex -FromMsBuild
 
-&"C:\UE4\Engine\Build\BatchFiles\RunUAT.bat" -ScriptsForProject=$ProjectFile BuildCookRun -project=$ProjectFile -noP4 -clientconfig=Shipping -serverconfig=Shipping -nocompileeditor -ue4exe=C:\UE4\Engine\Binaries\Win64\UE4Editor-Cmd.exe -utf8output -platform=Win64 -build -cook -map= -unversionedcookedcontent -compressed -stage -package -Messaging -archive -archivedirectory=$ArchiveDir -compile
+Start-Process -FilePath "C:\UE4\Engine\Build\BatchFiles\RunUAT.bat" -ArgumentList "-ScriptsForProject=$ProjectFile BuildCookRun -project=$ProjectFile -noP4 -clientconfig=Debug -serverconfig=Debug -ue4exe=C:\UE4\Engine\Binaries\Win64\UE4Editor-Cmd.exe -utf8output -platform=Win64 -build -cook -map= -unversionedcookedcontent -compressed -stage -package -Messaging -archive -archivedirectory=$ArchiveDir -compile" -Wait -NoNewWindow
 
 # Compress build artifacts
-Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList "C:\archive.zip C:\archive" -Wait
+Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList "a C:\archive.zip C:\archive" -Wait -NoNewWindow
 
 $ts = Get-Date -UFormat %s -Millisecond 0
 $filePath = "testgame-{0}.zip" -f $ts
