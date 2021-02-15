ab3183be034f4cc23af1e041804ac8170a6730d4
esplo <esplo@users.noreply.github.com>
2021-02-15 16:18:09 +0900

    refactor AWS Backup policy

--- lib/constructs/backup.ts
+++ lib/constructs/backup.ts
@@ -16,98 +16,14 @@ export class BackupPattern extends cdk.Construct {
       assumedBy: new ServicePrincipal("backup.amazonaws.com"),
     });
 
-    // ref: https://docs.aws.amazon.com/aws-backup/latest/devguide/access-control.html
-    const EBSBackupPolicy = new iam.Policy(scope, "EBSBackupPolicy", {
-      statements: [
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          resources: ["*"],
-          actions: [
-            "ssmmessages:CreateControlChannel",
-            "ssmmessages:CreateDataChannel",
-            "ssmmessages:OpenControlChannel",
-            "ssmmessages:OpenDataChannel",
-            "ssm:UpdateInstanceInformation",
-          ],
-        }),
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          resources: ["arn:aws:ec2:*::snapshot/*", "arn:aws:ec2:*:*:volume/*"],
-          actions: ["ec2:CreateSnapshot", "ec2:DeleteSnapshot"],
-        }),
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          resources: ["*"],
-          actions: ["ec2:DescribeVolumes", "ec2:DescribeSnapshots"],
-        }),
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          resources: ["*"],
-          actions: ["tag:GetResources"],
-        }),
-      ],
-    });
-    backupRole.attachInlinePolicy(EBSBackupPolicy);
-    const EC2BackupPolicy = new iam.Policy(scope, "EC2BackupPolicy", {
-      statements: [
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          resources: ["arn:aws:ec2:*::snapshot/*"],
-          actions: ["ec2:CreateTags", "ec2:DeleteSnapshot"],
-        }),
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          resources: ["*"],
-          actions: ["ec2:CreateImage", "ec2:DeregisterImage"],
-        }),
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          resources: ["*"],
-          actions: ["ec2:CopyImage", "ec2:CopySnapshot"],
-        }),
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          resources: ["arn:aws:ec2:*:*:image/*"],
-          actions: ["ec2:CreateTags"],
-        }),
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          resources: ["*"],
-          actions: [
-            "ec2:DescribeSnapshots",
-            "ec2:DescribeTags",
-            "ec2:DescribeImages",
-            "ec2:DescribeInstances",
-            "ec2:DescribeInstanceAttribute",
-            "ec2:DescribeInstanceCreditSpecifications",
-            "ec2:DescribeNetworkInterfaces",
-            "ec2:DescribeElasticGpus",
-            "ec2:DescribeSpotInstanceRequests",
-          ],
-        }),
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          resources: ["arn:aws:ec2:*::snapshot/*", "arn:aws:ec2:*:*:volume/*"],
-          actions: [
-            "ec2:CreateSnapshot",
-            "ec2:DeleteSnapshot",
-            "ec2:DescribeVolumes",
-            "ec2:DescribeSnapshots",
-          ],
-        }),
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          resources: ["*"],
-          actions: ["tag:GetResources"],
-        }),
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          resources: ["arn:aws:backup:*:*:backup-vault:*"],
-          actions: ["backup:DescribeBackupVault", "backup:CopyIntoBackupVault"],
-        }),
-      ],
-    });
-    backupRole.attachInlinePolicy(EC2BackupPolicy);
+    backupRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AWSBackupOperatorAccess")
+    );
+    backupRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSBackupServiceRolePolicyForBackup"
+      )
+    );
 
     const valutKey = new kms.Key(scope, "ValutDefaultKey", {
       enableKeyRotation: true,
@@ -117,21 +33,25 @@ export class BackupPattern extends cdk.Construct {
       backupVaultName: "game-studio-backup-vault",
       encryptionKeyArn: valutKey.keyArn,
     });
-    const backupPlan = new backup.CfnBackupPlan(scope, "game-studio-ebs-backup", {
-      backupPlan: {
-        backupPlanName: "game-studio-ebs-backup",
-        backupPlanRule: [
-          {
-            ruleName: "daily-10days-retention",
-            targetBackupVault: valut.backupVaultName,
-            scheduleExpression: "cron(0 12 * * ? *)",
-            lifecycle: {
-              deleteAfterDays: 10,
+    const backupPlan = new backup.CfnBackupPlan(
+      scope,
+      "game-studio-ebs-backup",
+      {
+        backupPlan: {
+          backupPlanName: "game-studio-ebs-backup",
+          backupPlanRule: [
+            {
+              ruleName: "daily-10days-retention",
+              targetBackupVault: valut.backupVaultName,
+              scheduleExpression: "cron(0 12 * * ? *)",
+              lifecycle: {
+                deleteAfterDays: 10,
+              },
             },
-          },
-        ],
-      },
-    });
+          ],
+        },
+      }
+    );
     backupPlan.addDependsOn(valut);
 
     new backup.CfnBackupSelection(scope, "game-studio-ebs-backup-selection", {
