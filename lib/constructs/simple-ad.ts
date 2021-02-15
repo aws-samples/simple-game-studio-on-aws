de89628a21996178622f04490d7132c4e68d2b20
esplo <esplo@users.noreply.github.com>
2021-02-15 16:25:51 +0900

    Add Active Directory support for Workstations

--- lib/constructs/simple-ad.ts
+++ lib/constructs/simple-ad.ts
@@ -4,14 +4,27 @@ import * as directoryService from "@aws-cdk/aws-directoryservice";
 import * as secretsmanager from "@aws-cdk/aws-secretsmanager";
 
 export class SimpleADPatternProps {
+  readonly name: string;
   readonly vpc: ec2.IVpc;
 }
 
 export class SimpleADPattern extends cdk.Construct {
-  readonly directory: directoryService.CfnSimpleAD;
+  readonly name: string;
+  readonly directoryOU: string;
+  readonly directoryId: string;
+  // If you need these values in other stacks
+  readonly dnsAddressesExportIds: string[];
+  // Caution: use this value only in the same stack!
+  readonly dnsIpAddresses: string[];
 
   constructor(scope: cdk.Construct, id: string, props: SimpleADPatternProps) {
     super(scope, id);
+
+    this.name = props.name;
+    this.directoryOU = this.name
+      .split(".")
+      .map((e) => `DC=${e}`)
+      .join(",");
 
     const adSecret = new secretsmanager.Secret(this, "SimpleADSecret", {
       generateSecretString: {
@@ -21,8 +34,8 @@ export class SimpleADPattern extends cdk.Construct {
       },
     });
 
-    this.directory = new directoryService.CfnSimpleAD(this, "StudioAD", {
-      name: "studio-ad.mycompany",
+    const directory = new directoryService.CfnSimpleAD(this, "StudioAD", {
+      name: this.name,
       password: adSecret.secretValueFromJson("password").toString(),
       size: "Small",
       vpcSettings: {
@@ -30,5 +43,17 @@ export class SimpleADPattern extends cdk.Construct {
         subnetIds: props.vpc.privateSubnets.map((e) => e.subnetId),
       },
     });
+
+    this.directoryId = directory.ref;
+
+    // to address this issue: https://github.com/aws/aws-cdk/issues/12523
+    this.dnsAddressesExportIds = ["StudioADDNS1", "StudioADDNS2"];
+    [0, 1].map((i) => {
+      new cdk.CfnOutput(this, this.dnsAddressesExportIds[i], {
+        value: cdk.Fn.select(i, directory.attrDnsIpAddresses),
+        exportName: this.dnsAddressesExportIds[i],
+      });
+    });
+    this.dnsIpAddresses = directory.attrDnsIpAddresses;
   }
 }
